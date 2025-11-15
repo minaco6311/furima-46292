cba956c282cd9799789c094ec4d9c3cacdcc97d6
minaco6311 <chiharu6311@gmail.com>
2025-11-15 17:36:45 +0900

    first commit

---
+++ app/assets/config/manifest.js
@@ -0,0 +1,6 @@
+//= link_tree ../images
+//= link_directory ../stylesheets .css
+//= link_tree ../../javascript .js
+//= link_tree ../../../vendor/javascript .js
+//= link application.css
+// 実装の注意点を確認
