ebcbe4b32fc22b1e5b02a449f78e8ea11e8ddbdf
minaco6311 <chiharu6311@gmail.com>
2025-12-09 02:41:15 +0900

    商品出品機能完成

---
+++ app/models/item.rb
@@ -0,0 +1,33 @@
+class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions   
+  
+  belongs_to :item_category
+  belongs_to :item_condition
+  belongs_to :shipping_cost
+  belongs_to :shipping_area
+  belongs_to :delivery_time
+
+  has_one_attached :image
+
+  belongs_to :user
+
+  has_one :order
+
+  with_options presence: true do
+    validates :image
+    validates :item_name
+    validates :item_detail
+    validates :price
+  end
+
+  validates :item_category_id, :item_condition_id, :shipping_cost_id,
+            :shipping_area_id, :delivery_time_id,
+            numericality: { other_than: 1, message: "can't be blank" }
+
+  validates :price, numericality: { 
+    only_integer: true,
+    greater_than_or_equal_to: 300,
+    less_than_or_equal_to: 9_999_999
+  }
+
+end
---
+++ spec/models/item_spec.rb
@@ -0,0 +1,91 @@
+require 'rails_helper'
+
+RSpec.describe Item, type: :model do
+  before do
+    @item = FactoryBot.build(:item) # 正常な初期値（Factoryの値）
+  end
+
+  describe '商品出品機能' do
+
+    context '出品できる時' do
+      it '全ての値が正しければ出品できる' do
+        expect(@item).to be_valid
+      end
+    end
+
+    context '出品できない時' do
+      it 'item_nameが空だと出品できない' do
+        @item.item_name = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Item name can't be blank")
+      end
+
+      it 'item_detailが空だと出品できない' do
+        @item.item_detail = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Item detail can't be blank")
+      end
+
+      it 'priceが空だと出品できない' do
+        @item.price = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank")
+      end
+
+      it '画像がないと出品できない' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
+      end
+
+      it 'カテゴリーのidが1（---）だと登録できない' do
+        @item.item_category_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Item category can't be blank")
+      end
+
+      it 'priceが300未満だと保存できない' do
+        @item.price = 299
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
+      end
+
+      it 'priceが9,999,999より大きいと保存できない' do
+        @item.price = 10_000_000
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
+      end
+
+      it 'priceが全角数字では保存できない' do
+        @item.price = '５００'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not a number")
+      end
+
+      it '商品の状態のidが1では保存できない' do
+        @item.item_condition_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Item condition can't be blank")
+      end
+
+      it '配送料の負担のidが1では保存できない' do
+        @item.shipping_cost_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping cost can't be blank")
+      end
+
+      it '発送元の地域のidが1では保存できない' do
+        @item.shipping_area_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping area can't be blank")
+      end
+
+      it '発送までの日数のidが1では保存できない' do
+        @item.delivery_time_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Delivery time can't be blank")
+      end
+
+    end
+  end
+end
