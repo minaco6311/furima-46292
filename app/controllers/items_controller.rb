a1a6c8675b69044149b47c43fdb716287f66eefd
minaco6311 <chiharu6311@gmail.com>
2025-12-14 15:55:04 +0900

    商品削除機能完成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,6 @@
 class ItemsController < ApplicationController
-  
   before_action :authenticate_user!, only: [:new, :create]
-  
+
   def index
     @items = Item.order(created_at: :desc)
   end
@@ -17,20 +16,25 @@ class ItemsController < ApplicationController
   def create
     @item = Item.new(item_params)
     if @item.save
-      redirect_to root_path   # 保存できたらトップへ
+      redirect_to root_path # 保存できたらトップへ
     else
-      render :new, status: :unprocessable_entity  # 失敗ならnewへ戻す
+      render :new, status: :unprocessable_entity # 失敗ならnewへ戻す
     end
+  end
+
+  def destroy
+    item = Item.find(params[:id])
+    item.destroy if item.user_id == current_user.id
+    redirect_to root_path
   end
 
   private
 
   def item_params
-  params.require(:item).permit(
-    :item_name, :item_detail, :price, :image,
-    :item_category_id, :item_condition_id,
-    :shipping_cost_id, :shipping_area_id, :delivery_time_id
-  ).merge(user_id: current_user.id)
-end
-
+    params.require(:item).permit(
+      :item_name, :item_detail, :price, :image,
+      :item_category_id, :item_condition_id,
+      :shipping_cost_id, :shipping_area_id, :delivery_time_id
+    ).merge(user_id: current_user.id)
+  end
 end
